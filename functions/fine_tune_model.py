39b34508288729825bc0dc39ecf472942e16309f
Mahmoud Sakka <mab.sakka@gmail.com>
2025-01-18 01:23:54 +0100

    implement token ana for init data

--- functions/fine_tune_model.py
+++ functions/fine_tune_model.py
@@ -27,3 +27,53 @@ def filter_rows_by_length(dataset, tokenizer, max_length=512):
     combined_lengths = [d + s + 3 for d, s in zip(doc_lengths, summ_lengths)]  # +3 for special tokens
     return dataset[[length <= max_length for length in combined_lengths]]
 
+
+
+# Training function
+def train_epoch(model, data_loader, optimizer, criterion, device):
+    model.train()
+    losses = []
+    correct_predictions = 0
+
+    for batch_idx, batch in enumerate(data_loader):
+        # Access the current batch index
+        print(f"Processing batch {batch_idx + 1}/{len(data_loader)}", end = '\r')
+
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['label'].to(device)
+
+        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+        logits = outputs.logits
+        loss = criterion(logits, labels)
+        losses.append(loss.item())
+
+        _, preds = torch.max(logits, dim=1)
+        correct_predictions += torch.sum(preds == labels)
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+    return correct_predictions.double() / len(data_loader.dataset), sum(losses) / len(losses)
+
+def eval_model(model, data_loader, criterion, device):
+    model.eval()
+    losses = []
+    correct_predictions = 0
+
+    with torch.no_grad():
+        for batch in data_loader:
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['label'].to(device)
+
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+            logits = outputs.logits
+            loss = criterion(logits, labels)
+            losses.append(loss.item())
+
+            _, preds = torch.max(logits, dim=1)
+            correct_predictions += torch.sum(preds == labels)
+
+    return correct_predictions.double() / len(data_loader.dataset), sum(losses) / len(losses)
